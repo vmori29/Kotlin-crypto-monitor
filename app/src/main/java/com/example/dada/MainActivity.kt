fdefdbb13e7a0c4668029fea8a307423b95cad8e
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-09 20:45:04 -0300

    first commit

---
+++ app/src/main/java/com/example/dada/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.dada
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val toolbarMain: Toolbar = findViewById(R.id.toolbar_main)
+        configureToolBar(toolbarMain)
+    }
+
+    private fun configureToolBar(toolbar: Toolbar){
+        setSupportActionBar(toolbar)
+        toolbar.setTitleTextColor(getColor(R.color.white))
+        supportActionBar?.setTitle(getText(R.string.app_title))
+        supportActionBar?.setBackgroundDrawable(getDrawable(R.color.primary))
+    }
+}
